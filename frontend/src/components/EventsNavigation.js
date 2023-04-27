0701dd59d75f8fed12a6abe8d5fc837ca832c267
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-27 10:50:55 -0300

    replacing classes for styles

--- frontend/src/components/EventsNavigation.js
+++ frontend/src/components/EventsNavigation.js
@@ -1,20 +1,20 @@
-import classes from './EventsNavigation.module.css';
+import styles from './EventsNavigation.module.css';
 
 function EventsNavigation() {
-  return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <a href="/events">All Events</a>
-          </li>
-          <li>
-            <a href="/events/new">New Event</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <nav>
+                <ul className={styles.list}>
+                    <li>
+                        <a href="/events">All Events</a>
+                    </li>
+                    <li>
+                        <a href="/events/new">New Event</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default EventsNavigation;
--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,20 +1,20 @@
-import classes from './MainNavigation.module.css';
+import styles from './MainNavigation.module.css';
 
 function MainNavigation() {
-  return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Events</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <nav>
+                <ul className={styles.list}>
+                    <li>
+                        <a>Home</a>
+                    </li>
+                    <li>
+                        <a>Events</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default MainNavigation;
