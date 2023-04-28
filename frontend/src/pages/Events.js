8dfc3590d1b3ff4879c7ec157abf1adbc0f00657
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-28 09:59:07 -0300

    Error Handling with Custom Errors

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,6 +3,11 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
     const data = useLoaderData();
+
+    // if (data.isError) {
+    //     return <p>{data.message}</p>;
+    // }
+
     const events = data.events;
 
     return <EventsList events={events} />;
@@ -14,7 +19,10 @@ export async function loader() {
     // We can not use useSession() here, because this not a React component. (only limitation)
     const response = await fetch('http://localhost:8080/events');
     if (!response.ok) {
-        // ...
+        // return { isError: true, message: 'Failed to load events' };
+        // throw new Error('Failed to load events');
+        // eslint-disable-next-line no-throw-literal
+        throw { message: 'Failed to load events' };
     } else {
         return response;
     }
